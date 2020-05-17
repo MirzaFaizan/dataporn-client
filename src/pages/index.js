952a2772fe31c01e5d2c81bdbfd600568996e2b6
Faizan <mirzafaizanejaz@gmail.com>
2020-05-17 15:02:21 +0300

    integrated apollo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,15 @@ import {graphql} from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import GridItem from "../components/gridItem"
+import {Query} from "react-apollo"
+import gql from "graphql-tag"
 import InfiniteScroll from "../components/infiniteScroll"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-      <div className="card-columns">
+      <div className="card-columns pt-5 mt-5">
         {data.wpql.posts.nodes.map((post, index) =>(
           <GridItem key={index} image={`https://api.dataporn.cc/wp-content/uploads/${post.featuredImage.mediaDetails.file}`} alt={post.featuredImage.slug} title={post.title}/>
         ))}
@@ -33,5 +35,23 @@ export const query = graphql`
       }
     }
   }`
+
+  export const APOLLO_QUERY = gql`
+  query myQuery {
+    wpql {
+      posts {
+        nodes {
+          featuredImage {
+            mediaDetails {
+              file
+            }
+            slug
+          }
+          title
+        }
+      }
+    }
+  }
+  `
 
 export default IndexPage
