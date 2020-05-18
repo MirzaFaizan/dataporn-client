2a6de497e2012fc3d1fbe045295e427c5744e2a8
Faizan <mirzafaizanejaz@gmail.com>
2020-05-18 13:08:58 +0300

    added infinite scroll

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,35 @@
-import React from "react"
-import {graphql} from "gatsby"
+import React, {useState} from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import GridItem from "../components/gridItem"
 import {useQuery} from "@apollo/react-hooks"
 import gql from "graphql-tag"
-// import InfiniteScroll from "../components/infiniteScroll"
+import useInfiniteScroll from "../components/useInfinteScroll"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 const IndexPage = () => {
-  const {loading, error, data} = useQuery(APOLLO_QUERY)
+  const [offset, setOffset] = useState(0);
+  const [isFetching, setIsFetching] = useInfiniteScroll(fetchMoreListItems);
+  const {loading, error, data} = useQuery(APOLLO_QUERY);
   if (loading) return `loading`
   if (error) return `error ${error}`
+
+  function fetchMoreListItems() {
+    setOffset(offset+10);
+    setIsFetching(false);
+  }
+
   return(
     <>
     <Layout>
     <SEO title="Home" />
       <div className="card-columns pt-5 mt-5">
-        {data.posts.nodes.map((post, index) =>{ return(
+
+      {data.posts.nodes.slice(0,offset+10).map((post, index) =>{ return(
           <GridItem key={index} image={`https://api.dataporn.cc/wp-content/uploads/${post.featuredImage.mediaDetails.file}`} alt={post.featuredImage.slug} title={post.title}/>
         )})}
-    </div>
+      </div>
+      {isFetching && 'Fetching more list items...'}
   </Layout>
   </>
   )
@@ -29,7 +38,7 @@ const IndexPage = () => {
 
   export const APOLLO_QUERY = gql`
   query MyQuery {
-    posts {
+    posts (first : 500){
       nodes {
         title
         id
