efd6c4ce43800330735988cfef8f3864cd8fd6f7
Faizan <mirzafaizanejaz@gmail.com>
2020-05-19 11:05:00 +0300

    added image gallery

--- src/components/gridItem.jsx
+++ src/components/gridItem.jsx
@@ -2,17 +2,17 @@ import React from "react"
 import share from "../images/sharepost.svg"
 import "./gridItem.css"
 
-const GridItem = ({ image, alt, title }) => {
+const GridItem = ({ image, alt, title , setImage, setOpenImage }) => {
     // Create gallery here
     return (
         <div className="card">
-            <img className="card-img-top" src={image} alt={alt}/>
+            <img className="card-img-top" src={image} onClick={()=>{setImage(image); setOpenImage(true)}} alt={alt}/>
                 <div className="card-body row">
                     <div className="col-10">
                         <p className="card-text font-weight-bold spacing">{title}</p>
                     </div>
                     <div className="col-2">
-                        <img src={share} alt="share" height="30" width="30"/>
+                        <img src={share}  alt="share" height="30" width="30"/>
                     </div>
             </div>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,14 @@ import gql from "graphql-tag"
 import useInfiniteScroll from "../components/useInfinteScroll"
 import "bootstrap/dist/css/bootstrap.min.css"
 import MyLoader from "../components/loader"
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css'; // This only needs to be imported once in your app
+
 
 const IndexPage = () => {
   const [offset, setOffset] = useState(0);
+  const [image, setImage] = useState(`https://via.placeholder.com/150`)
+  const [openImage, setOpenImage] = useState(false)
   const [isFetching, setIsFetching] = useInfiniteScroll(fetchMoreListItems);
   const {loading, error, data} = useQuery(APOLLO_QUERY);
   
@@ -28,29 +33,36 @@ const IndexPage = () => {
       {
       loading ?
         <div>
-          <div className="col">
-            <MyLoader/>
+          <div className="col mx-auto">
+            <center><MyLoader/></center>
           </div>
-          <div className="col">
-            <MyLoader/>
+          <div className="col mx-auto">
+            <center><MyLoader/></center>
           </div>
-          <div className="col">
-            <MyLoader/>
+          <div className="col mx-auto">
+            <center><MyLoader/></center>
           </div>
-          <div className="col">
-            <MyLoader/>
+          <div className="col mx-auto">
+            <center><MyLoader/></center>
           </div>
-          <div className="col">
-            <MyLoader/>
+          <div className="col mx-auto">
+            <center><MyLoader/></center>
           </div>
         </div>:
         data.posts.nodes.slice(0,offset+10).map((post, index) =>{ return(
-          <GridItem key={index} image={`https://api.dataporn.cc/wp-content/uploads/${post.featuredImage.mediaDetails.file}`} alt={post.featuredImage.slug} title={post.title}/>
+          <GridItem key={index} image={`https://api.dataporn.cc/wp-content/uploads/${post.featuredImage.mediaDetails.file}`} setImage={setImage} setOpenImage={setOpenImage}  alt={post.featuredImage.slug} title={post.title}/>
         )})}
       </div>
       {isFetching && <div className="col">
             <MyLoader/>
           </div>}
+
+      {openImage && (
+      <Lightbox
+        mainSrc={image}
+        onCloseRequest={() => setOpenImage(false)}
+      />
+    )}
   </Layout>
   </>
   )
